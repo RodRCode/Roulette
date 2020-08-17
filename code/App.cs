6f1ae3de285211aaf145cf748c5cc7ec18c6030d
Rod' Rodriguez <Rod.R.Code@gmail.com>
2020-08-17 12:15:00 -0500

    Added the list of things to do for the Roulette program as well as a stretch goal

---
+++ code/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Roulette
+{
+    internal class App
+    {
+        public App()
+        {
+        }
+
+        internal void Run()
+        {
+            //generate random numbers from 0 to 37.  0 is going to be "0" on the wheel, 37 is going to be "00" on the wheel
+            //assign 18 of the numbers to a color with the other 18 being the other color
+            //stretch goal generate an onscreen display that has the numbers spinning around with a beep, would be extra cool if the number picked was actually the one that it choose at the end of the final beep.  The beeps should slow down as it gets closer to finishing the drop.
+            //Implement the different bets
+            //1. the number of the bin
+            //2. Evens/Odds
+            //3. Reds/Blacks
+            //4. Lows/Highs (1-18) (19-36)
+            //5. Dozens (1-12) (13-24) (25-36)
+            //6. Columns: first second or third
+            //7. Streets: rows 1/2/3 or 22/23/24
+            //8. 6 Numbers: Double rows 1/2/3/4/5/6 or 22/23/24/25/26/27
+            //9. Split the edge of any two contiguoius numbers 1/2, 11/14, 35/36
+            //10. Corners: 1/2/4/5 or 23/24/26/27 
+        }
+    }
+}
--- code/Program.cs
+++ code/Program.cs
@@ -6,7 +6,7 @@ namespace Roulette
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new App().Run();
         }
     }
 }
